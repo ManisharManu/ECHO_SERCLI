1b1dd3d9754fa22521041d96d05d71dd69ad4427
Manisha <rlmanus31@gmail.com>
2018-03-23 11:32:49 +0530

    client updated

--- ECclient.java
+++ ECclient.java
@@ -3,3 +3,12 @@ import java.net.*;
 
 public class EClient
 {
+public static void main(String[] args)
+	{
+		try
+    {
+	
+       Socket s = new Socket("127.0.0.1", 9999);
+			BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			PrintWriter w = new PrintWriter(s.getOutputStream(), true);
+			BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
